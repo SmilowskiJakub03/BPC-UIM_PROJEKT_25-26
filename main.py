0e215bf1fe86707b58747b5e0012dffaeeca24ce
jakub.rehurek <124621193+kubous2@users.noreply.github.com>
2025-10-01 20:10:36 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+
+"""
+Created on 11. 09. 2025 at 11:13:56
+
+Author: Richard Redina
+Email: 195715@vut.cz
+Affiliation:
+         International Clinical Research Center, Brno
+         Brno University of Technology, Brno
+GitHub: RicRedi
+
+(._.)
+ <|>
+_/|_
+
+Description:
+    Tento script slouží jako hlavní spouštěcí bod pro projekt.
+    Skript berte jako volný rámec, který můžete upravit dle svých potřeb.    
+"""
+# Import necessary modules
+import numpy as np
+# My model
+
+def load_data(
+    # Add your parameters here....
+    ) -> np.ndarray:
+    """
+    Function to load your data.
+
+    Parameters
+    ----------
+    Add your parameters here....
+    """
+    # Load and return your data
+    raise NotImplementedError("Function load_data() is not implemented.")
+
+def data_preprocessing(
+    # Add your parameters here....
+    # data: np.ndarray = None
+    ) -> np.ndarray:
+    """
+    Function to preprocess your data.
+
+    Parameters
+    ----------
+    Add your parameters here....
+    """
+    # Preprocess and return your data
+    raise NotImplementedError("Function data_preprocessing() is not implemented.")
+
+def my_model(
+    # Add your parameters here....
+    ) -> np.ndarray:
+    """
+    Class representing your model.
+    """
+    raise NotImplementedError("Function my_model() is not implemented.")
+
+def main(
+    # Add your parameters here....
+    ) -> None:
+    """
+    Main function to run the project.
+
+    Parameters
+    ----------
+    Add your parameters here....
+    """
+    # Initialize and run your model
+
+    # model = my_model(
+    #     # Pass your parameters here....
+    # )
+
+    # Add your code here....
+
+    # Print Matthews Correlation Coefficient (MCC)
+    # print(f"Matthews Correlation Coefficient (MCC): {matthews_corrcoef}")
+    raise NotImplementedError("Function main() is not implemented.")
+
+def compute_statistics(
+    # Add your parameters here....
+    # model_output: np.ndarray = None,
+    # ground_truth: np.ndarray = None
+    ) -> tuple:
+    """
+    Function to compute statistics.
+
+    Parameters
+    ----------
+    Add your parameters here....
+    """
+    # Compute Matthews Correlation Coefficient (MCC)
+    # matthews_corrcoef = None
+    raise NotImplementedError("Function compute_statistics() is not implemented.")
+
+if __name__ == "__main__":
+    main(
+        # Pass your parameters here....
+    )
---
+++ testing.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+"""
+Created on 11. 09. 2025 at 11:33:18
+
+Author: Richard Redina
+Email: 195715@vut.cz
+Affiliation:
+         International Clinical Research Center, Brno
+         Brno University of Technology, Brno
+GitHub: RicRedi
+
+(._.)
+ <|>
+_/|_
+
+Description:
+    Tento script slouží k testování vašeho modelu.
+    V rámci testování byste měli vytvořit funkci, která načte data a model,
+    provede testování a vypíše výsledky včetně matice záměny a mathews correlation coefficient.
+"""
+# Import necessary modules
+import numpy as np
+
+def test_model(
+    # Add your parameters here....
+    ) -> np.ndarray:
+    """
+    Function to test your model.
+
+    Parameters
+    ----------
+    Add your parameters here....
+    """
+    # Test and return your results
+    # load data
+    # load model
+
+    # test_results = None
+
+    # compute metrics
+    # print confusion matrix
+    # print mathews correlation coefficient
+    raise NotImplementedError("Function test_model() is not implemented.")
+
+if __name__ == "__main__":
+    test_model()
